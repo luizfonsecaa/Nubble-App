0e12a77bbcc950aba0e17c117397d4b29970a438
Luiz Fonseca <luizfonseca@Mac-mini-de-Luiz.local>
2025-12-01 20:30:05 -0300

    feat: added new component icons

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
 import { View } from 'react-native'
 import { Text } from './src/components/Text/Text'
-import { Button } from './src/components/Button/Button'
+
 import { ThemeProvider } from '@shopify/restyle'
 import { theme } from './src/theme/theme'
+import { Icon } from './src/components/Icon/Icon'
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <View style={{ marginTop: 50, marginHorizontal: 30 }}>
         <Text preset="headingLarge">Olá</Text>
-        <Button title="Entrar" mb="s12" />
-        <Button loading mb="s12" title="Entrar" preset="outline" />
-        <Button title="Entrar" disable />
+        <Icon name="eyeOff" size={30} />
       </View>
     </ThemeProvider>
   )
